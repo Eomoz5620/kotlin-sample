510b46624621b89bebf3d78e0489c82b49ca35f5
Eomoz5620 <benjaminlevesque4@gmail.com>
2025-04-23 14:56:41 +0200

    🔐 Secret

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
     println("Hello, this is a vulnerable Kotlin app.")
+
+    // 🚨 Faux secret (clé API exposée)
+    val apiKey = "AKIAFAKESECRET1234567890"
+    println("Using API key: $apiKey")
 }
